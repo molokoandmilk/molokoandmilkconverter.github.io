2ff73b2845d7034ca01e270cbd96ef37c7eb356a
molokoandmilk <106869516+molokoandmilk@users.noreply.github.com>
2023-06-08 16:10:01 +0300

    new converter

---
+++ js/state.js
@@ -0,0 +1,19 @@
+export default {
+    url: 'https://v6.exchangerate-api.com/v6/0f38bacc8af066ede8267b72',
+    codes: [],
+    pair: {
+      from: "",
+      to: "",
+    },
+    amount: "",
+    loading: false,
+    currentTab: "convert",
+    currency: {
+      code: "USD",
+    },
+    currencies: ["USD", "EUR", "BYN"],
+    actions: {
+      remove: "remove",
+      change: "change",
+    },
+  };
